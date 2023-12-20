3dc724bf99c012fbddf7557931ae943962e385ad
Bob <bob@air>
2023-12-20 15:22:36 +0800

    Init

---
+++ src/main/java/com/thehecklers/sburrestdemo/SburRestDemoNameApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.sburrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburRestDemoNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburRestDemoNameApplication.class, args);
+	}
+
+}
